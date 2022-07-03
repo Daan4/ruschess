27337bb8140b8d037be246741e10b1f205ad1f6f
Daan4 <Daan4@users.noreply.github.com>
2022-07-03 13:05:54 +0200

    basic frfamework, to implement move and sees

--- src/piece.rs
+++ src/piece.rs
@@ -1,18 +1,28 @@
 use crate::position::Position;
+use crate::game::Side;
 
 pub trait Piece {
+    // Move a piece, returns Err if the destination is invalid
     fn r#move(&mut self, destination: Position) -> Result<(), String>;
+
+    // Check if a piece sees a position, returns Err if the position is invalid
+    fn sees(&self, position: Position, pieces: Vec<Box<dyn Piece>>) -> Result<(), String>;
+
+    fn position(&self) -> Position;
+
+    fn side(&self) -> Side;
 }
 
 pub struct King {
     position: Position,
     captured: bool,
-    may_castle: bool
+    may_castle: bool,
+    side: Side,
 }
 
 impl King {
-    pub fn new(position: Position) -> King {
-        King{position, captured: false, may_castle: true}
+    pub fn new(position: Position, side: Side) -> King {
+        King{position, captured: false, may_castle: true, side}
     }
 }
 
@@ -28,17 +38,29 @@ impl Piece for King {
             return Ok(())
         }
     }
+
+    fn sees(&self, position: Position, pieces: Vec<Box<dyn Piece>>) -> Result<(), String> {
+        Ok(())
+    }
+
+    fn position(&self) -> Position {
+        self.position
+    }
+
+    fn side(&self) -> Side {
+        self.side
+    }
 }
 
 pub struct Queen {
     position: Position,
     captured: bool,
-    may_castle: bool
+    side: Side,
 }
 
 impl Queen {
-    pub fn new(position: Position) -> Queen {
-        Queen{position, captured: false, may_castle: false}
+    pub fn new(position: Position, side:Side) -> Queen {
+        Queen{position, captured: false, side}
     }
 }
 
@@ -46,17 +68,30 @@ impl Piece for Queen {
     fn r#move(&mut self, destination: Position) -> Result<(), String> {
         Ok(())
     }
+
+    fn sees(&self, position: Position, pieces: Vec<Box<dyn Piece>>) -> Result<(), String> {
+        Ok(())
+    }
+
+    fn position(&self) -> Position {
+        self.position
+    }
+
+    fn side(&self) -> Side {
+        self.side
+    }
 }
 
 pub struct Rook {
     position: Position,
     captured: bool,
-    may_castle: bool
+    may_castle: bool,
+    side: Side,
 }
 
 impl Rook {
-    pub fn new(position: Position) -> Rook {
-        Rook{position, captured: false, may_castle: true}
+    pub fn new(position: Position, side: Side) -> Rook {
+        Rook{position, captured: false, may_castle: true, side}
     }
 }
 
@@ -64,17 +99,29 @@ impl Piece for Rook {
     fn r#move(&mut self, destination: Position) -> Result<(), String> {
         Ok(())
     }
+
+    fn sees(&self, position: Position, pieces: Vec<Box<dyn Piece>>) -> Result<(), String> {
+        Ok(())
+    }
+
+    fn position(&self) -> Position {
+        self.position
+    }
+
+    fn side(&self) -> Side {
+        self.side
+    }
 }
 
 pub struct Bishop {
     position: Position,
     captured: bool,
-    may_castle: bool
+    side: Side,
 }
 
 impl Bishop {
-    pub fn new(position: Position) -> Bishop {
-        Bishop{position, captured: false, may_castle: false}
+    pub fn new(position: Position, side: Side) -> Bishop {
+        Bishop{position, captured: false, side}
     }
 }
 
@@ -82,17 +129,29 @@ impl Piece for Bishop {
     fn r#move(&mut self, destination: Position) -> Result<(), String> {
         Ok(())
     }
+
+    fn sees(&self, position: Position, pieces: Vec<Box<dyn Piece>>) -> Result<(), String> {
+        Ok(())
+    }
+
+    fn position(&self) -> Position {
+        self.position
+    }
+
+    fn side(&self) -> Side {
+        self.side
+    }
 }
 
 pub struct Knight {
     position: Position,
     captured: bool,
-    may_castle: bool
+    side: Side,
 }
 
 impl Knight {
-    pub fn new(position: Position) -> Knight {
-        Knight{position, captured: false, may_castle: false}
+    pub fn new(position: Position, side: Side) -> Knight {
+        Knight{position, captured: false, side}
     }
 }
 
@@ -100,22 +159,47 @@ impl Piece for Knight {
     fn r#move(&mut self, destination: Position) -> Result<(), String> {
         Ok(())
     }
+
+    fn sees(&self, position: Position, pieces: Vec<Box<dyn Piece>>) -> Result<(), String> {
+        Ok(())
+    }
+
+    fn position(&self) -> Position {
+        self.position
+    }
+
+    fn side(&self) -> Side {
+        self.side
+    }
 }
 
 pub struct Pawn {
     position: Position,
     captured: bool,
-    may_castle: bool
+    has_moved: bool,
+    side: Side,
 }
 
 impl Pawn {
-    pub fn new(position: Position) -> Pawn {
-        Pawn{position, captured: false, may_castle: false}
+    pub fn new(position: Position, side: Side) -> Pawn {
+        Pawn{position, captured: false, has_moved: false, side}
     }
 }
 
 impl Piece for Pawn {
     fn r#move(&mut self, destination: Position) -> Result<(), String> {
         Ok(())
+    }
+
+    fn sees(&self, position: Position, pieces: Vec<Box<dyn Piece>>) -> Result<(), String> {
+        Ok(())
+    }
+
+    fn position(&self) -> Position {
+        self.position
+    }
+
+    fn side(&self) -> Side {
+        self.side
     }
 }
